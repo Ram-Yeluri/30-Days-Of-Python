9d685be15dab3dc5e52daec9821f3c8e596a04f8
Ram <74450636+Ram-Yeluri@users.noreply.github.com>
2026-09-19 09:08:02 +0530

    Update helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -22,4 +22,4 @@ print(type(1 + 3j))              # Complex
 print(type('Ram'))          # String
 print(type([1, 2, 3]))           # List
 print(type({'name': 'Ram'}))  # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Tuple
+print(type({9.8, 3.14, 2.7}))    # Set
