9fd969ff2fc40b51824266bd0de79f2fb1973618
Ram <74450636+Ram-Yeluri@users.noreply.github.com>
2026-09-19 09:08:55 +0530

    Uncomment type checks for string and dictionary

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -19,7 +19,8 @@ print(3 // 2)  # Floor division operator(//)
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Ram'))          # String
+print(type('Ram'))               # String
 print(type([1, 2, 3]))           # List
-print(type({'name': 'Ram'}))  # Dictionary
+print(type({'name': 'Ram'}))     # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # tuple
